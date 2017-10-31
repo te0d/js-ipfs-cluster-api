f564df5b5d5e8da2918059556aef4128575396d8
Tom O'Donnell <todonnel91@gmail.com>
2017-10-31 17:25:48 +0000

    Added README and LICENSE
    Added README.md based on specification of `RichardLitt/standard-readme`.
    
    Added MIT License.
    
    Modified `index.js` to allow module configuration to accept host,
    port, and options parameters.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const getConfig = require('./utils/default-config')
 const getRequestAPI = require('./utils/request-api')
 const loadCommands = require('./utils/load-commands')
 
-function IpfsClusterAPI() {
+function IpfsClusterAPI(host, port, opts) {
   const config = getConfig()
-  
+
+  config.host = host || config.host;
+  config.port = port || config.port;
+  Object.assign(config, opts)
+
   const requestAPI = getRequestAPI(config)
   const cmds = loadCommands(requestAPI)
   cmds.send = requestAPI
